55aca5a67e6f6472d586421acaf8ff86475b5836
nate2402 <nate2402@ajou.ac.kr>
2025-05-15 01:02:26 +0900

    feat: 인스턴스 설정모듈 추가 (t2.micro)

---
+++ instance/t2.micro/main.tf
@@ -0,0 +1,18 @@
+# Instance Config Module: t2.micro
+
+resource "aws_instance" "t2_instance" {
+  ami = "ami-0943317d4313c0f89"
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [ var.security_group_id ]
+  
+  user_data = file("./scripts/t2.micro.sh")
+
+  tags = {
+    Name = "[Amate] " + var.instance_name
+    Project = "Amate"
+    Type = "EC2/t2.micro"
+  }
+}
