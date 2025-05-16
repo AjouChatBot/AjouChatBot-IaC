cc89b648da09deb7beb1645340622b95f151d0b4
nate2402 <nate2402@ajou.ac.kr>
2025-05-16 22:15:12 +0900

    fix: 오타수정

--- terraform/key/main.tf
+++ terraform/key/main.tf
@@ -1,5 +1,5 @@
 # Keypair Config Module
 resource "aws_key_pair" "Amate-Instance-Key" {
   key_name = "amate_key"
-  public_key = file("./.ssh/amate_ecdsa_key.pub")
+  public_key = file("../.ssh/amate_ecdsa_key.pub")
 }
